9059e057c879ff635abf06160c3199fd5925c659
y1799 <y1799.hotmail.com>
2021-07-30 19:30:11 -0400

    first commit

---
+++ src/test/day1.java
@@ -0,0 +1,19 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class day1 {
+
+	@Test
+	public void Demo() {
+		System.out.println("Hello");
+
+	}
+
+	@Test
+	public void Test2() {
+		System.out.println("Bye");
+
+	}
+
+}
